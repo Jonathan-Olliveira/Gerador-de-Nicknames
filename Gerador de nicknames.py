c99e6450b8c4286a11ac63ae0e6ec1235d8b7808
Jonathan Oliveira <125706491+Jonathan-Olliveira@users.noreply.github.com>
2024-05-28 23:23:19 -0300

    Add files via upload
    • Case sensitive ok
    • Criado funções para melhorar a performance do código
    • Começando a criar tratamento de erros no código

--- Gerador de nicknames.py
+++ Gerador de nicknames.py
@@ -1,6 +1,9 @@
 import random as rd
 import time
 
+#criar novas listas de nomes, inclusive separados que se concatenam.
+#mais opções de listas
+
 
 Brasileiro = [
     "GuerreiroBR", "CaçadorDeNoobs", "MestreDasLâminas", "FúriaTropical",
@@ -22,32 +25,43 @@ Estrangeiro = [
 ]
 #print(rd.choice(names))
 
+#Criar embaralhamento de lista
+def cria_nick():
+    for i in range(qtde):
+        if Estilo == 1:
+            print("Nick", i + 1, ":", rd.choice(Brasileiro))
+            time.sleep(1)
+        elif Estilo == 2:
+            print("Nick", i + 1, ":", rd.choice(Estrangeiro))
+            time.sleep(1)
+        else:
+            print("Valor não encontrado")
 
+
+#loop
 print("Bem-Vindo ao gerador de Nicknames!")
+while True:
 
-print("Deseja gerar quantos nomes por rodada?")
-rodada = int(input())
-print("Diga qual estilo de nick você prefere: ")
+    print("Deseja gerar quantos nomes?")
+    qtde = int(input())
+    print("Diga qual estilo de nick você prefere: ")
 
-print("""
-1- Brasileiro
-2- Estrangeiro
-3- Medievais
-4- Fodastico
-""")
-Estilo = int(input())
-
-for i in range(rodada):
-    if Estilo == 1:
-        print("Nick",i+1,":",rd.choice(Brasileiro))
-        time.sleep(1)
-    elif Estilo == 2:
-        print("Nick",i+1,":",rd.choice(Estrangeiro))
-        time.sleep(1)
-    else:
-        print("Valor não encontrado")
-
-
-
-
-
+    print("""
+    1- Brasileiro
+    2- Estrangeiro
+    3- Medievais
+    4- Fodastico
+    """)
+    Estilo = int(input())
+    cria_nick()
+    loop= str(input("Deseja Criar mais dos mesmo modo?"))
+    #Arrumar tratamento de erros caso não for uma string
+    #Arrumar variavel loop para repertir sem voltar ao inicio
+    #criar novos caminhos de loop, podendo alterar quantidade criada e modo do nick
+    try:
+        if loop.lower() == "sim" or "s":
+            cria_nick()
+    except ValueError:
+        print("Aceito somente texto")
+   # else:
+        #break
